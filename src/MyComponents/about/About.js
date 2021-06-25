cc64f288400a5ddf5cf4d006f1745e28aa0b12c5
Sarthak <sarthakbhatt22601@gmail.com>
2021-06-25 19:49:45 +0530

    Change is about.js

--- src/MyComponents/about/About.js
+++ src/MyComponents/about/About.js
@@ -5,7 +5,7 @@ export const About = () =>{
         <div>
             <div style={{textAlign:'center'}}>
                 <h1 style={{color:'red'}}>Developer : Sarthak Bhatt</h1>
-                <p>This is My first projecct in ReactJs. In this project I learn about different types of components in React, props & 
+                <p>This is My first project in ReactJs. In this project I learn about different types of components in React, props & 
                 states , proptype , different Hooks i.e. useState & useEffect.<br></br>I also Learn about routing concept.
                 For any suggestions you can visit my GitHub repo.<br></br>Thanks for visiting !!!</p>
             </div>    
